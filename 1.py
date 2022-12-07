865ee715aa1439ac711b257c393a614121551349
Frknzdmr1 <ozdemirf97@icloud.com>
2022-12-06 21:48:40 -0500

    V1

---
+++ 1.py
@@ -0,0 +1,47 @@
+# print("Hello World")
+
+# print("3.14")
+
+# print('p', 'y', 't', 'h', 'o', 'n', sep='-')
+# print('python', end='?')
+# print('Henry\tWilliam')
+
+# def myFunction():
+#     """_summary_
+#         This is the function docstring"""
+#     print(myFunction.__doc__)
+
+
+
+#     ## Order of Priority
+#     # not, and, or 
+
+#     print(3 and "Clarusway" and 22 and [1, 2, 3]) # and will return the always the last one if everything is true if its false it will return the first one
+#     print(0 or [] '' 22 or 'text') # or is returning the first truthy value
+#     print(0 or [] '') #outo\put is the value in ' '
+
+
+
+    # Indexing String 
+
+    # city [start:stop:step]
+
+    #stop is not included 
+
+
+
+    # String formating 
+
+    # fruit = 'orange'
+    # vegetable = ' tomato'
+    # amount = 6 
+
+    # print('the amount of{fruit} and {vegetable} we bought are totally {amount} pounds')
+   
+
+   # searching a string 
+
+   # (in) and (not in)  
+
+
+    
